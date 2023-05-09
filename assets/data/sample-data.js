42db2831eaa78e7134788ad12713a8b85cd5dbf8
Victor chikezie <vchikezie50@gmail.com>
2023-05-09 15:59:42 +0100

    this is a new change

---
+++ assets/data/sample-data.js
@@ -0,0 +1,9 @@
+export const sampleData= [
+    {id:2,email:'vchikezie@gmail.com',amount:80000,time:'12'},
+    {id:7,email:'chikezie@gmail.com',amount:67000,time:'15'},
+    {id:8,email:'victor@gmail.com',amount:12500,time:'30'},
+    {id:1,email:'chikezie50e@gmail.com',amount:2000,time:'31'},
+    {id:3,email:'victor@gmail.com',amount:200,time:'39'},
+    {id:4,email:'victorC@gmail.com',amount:12800,time:'39'},
+    {id:16,email:'chidiebere',amount:15000,time:'48'},  
+];
