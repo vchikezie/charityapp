f4f47bb6778437743a598c624870a44284373304
Victor chikezie <vchikezie50@gmail.com>
2023-06-12 16:52:43 +0100

    profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,45 +1,92 @@
-import { useContext,useEffect, useState } from "react";
-import { View,StyleSheet } from "react-native";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../sittings/globalVariables";
+import { StyleSheet,SafeAreaView,Image,View,Text} from "react-native";
 import { SafeArea } from "../components/SafeArea";
 import { Theme } from "../utils/Theme";
 import { Button } from "react-native-paper";
-import { db } from "../sittings/FireBase.sitting";
+import {db }  from "../sittings/FireBase.sitting"
 import { getDoc,doc } from "firebase/firestore";
 
+
 export function Profile ({navigation}) {
-    const {uid} = useContext(AppContext);
-    //update usestate after data is fetched
-    const [userRecords,setUserRecords] = useState({});
+    const { uid} = useContext(AppContext);
+    const [userRecords,setUserRecords] = useState({})
 
-    console.log(uid);
-
-    //fetch data after component is loaded
+    //fetch dATA AFTER COMPONENT IS LOADED
+    //console.log(uid);
     useEffect(() => {
         const handleGetUserRecords = async () => {
-            const snapShot = await getDoc(doc(db,'users',uid));
-
-            setUserRecords(snapShot.data());
+            const snapShot = await getDoc (doc(db,'users',uid))
+            setUserRecords(snapShot.data())
         }
         handleGetUserRecords();
-
-    }, []);
-   // console.log(userRecords);//delete after testing
-
-    
+    },[])
+    //console.log(userRecords);//delete after testing
     return (
-        <SafeArea>
-            <View >
-
+       <SafeAreaView style={styles.container}>
+       
+       
+         <Image
+         style={{
+            height:'100%',
+            width:'300%',
+            resizeMode:'contain',
+           alignSelf:'center',
+           
+         }}
+         source={{uri:'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}}/>   
+       
+        
+         
+         <View style={{backgroundColor:'white',marginHorizontal:12,marginBottom:12,borderRadius:8,padding:25,borderWidth:1,borderColor:Theme.colors.gray100,marginTop:10,}}>
+            <View style={{paddingLeft:9}}>
+            <Text style={{fontWeight:'bold'}}>Name :</Text>
+            <Text style={{fontWeight:'bold',fontSize:28}}>{userRecords.firstName} {userRecords.lastName}</Text> 
             </View>
-        </SafeArea>
+         </View>
+
+
+         <View style={{backgroundColor:'white', marginHorizontal:12,marginBottom:12,borderRadius:8,padding:25,borderWidth:1,borderColor:Theme.colors.gray100}}>
+            <View style={{paddingLeft:9}}>
+            <Text style={{fontWeight:'bold'}}>Date Of Birth :</Text>
+            <Text style={{fontWeight:'bold',fontSize:28}}>{userRecords.dateOfBirth}</Text>
+            </View>
+         </View>
+
+         <View style={{backgroundColor:'white',marginHorizontal:12,marginBottom:12,borderRadius:8,padding:25,borderWidth:1,borderColor:Theme.colors.gray100}}>
+            <View style={{paddingLeft:9}}>
+            <Text style={{fontWeight:'bold'}}>City :</Text>
+            <Text style={{fontWeight:'bold',fontSize:28}}>{userRecords.city}</Text>
+            </View>
+         </View>
+
+         <View style={{backgroundColor:'white',marginHorizontal:12,marginBottom:12,borderRadius:8,padding:25,borderWidth:1,borderColor:Theme.colors.gray100}}>
+            <View style={{paddingLeft:9}}>
+            <Text style={{fontWeight:'bold'}}>Bio :</Text>
+            <Text style={{fontWeight:'bold',fontSize:28}}>{userRecords.bioInfo}</Text>
+            </View>
+         </View>
+
+
+         <View style={{marginHorizontal:12}}>
+         <Button
+         contentStyle={{paddingVertical:16}}
+         style={{borderRadius:6}} 
+         textColor="white"
+         buttonColor="#19A7CE"
+         onPress={() => navigation.navigate('Create Profile')}>
+            Update profile
+         </Button>
+         </View>
+
+       </SafeAreaView>
     )
 }
 
 const styles=StyleSheet.create({
     container:{
-        flex:1,
-        backgroundColor:Theme.colors.lime100,
+        flex:0.3,
+        backgroundColor:Theme.colors.lime300,
     }
   
 })
