931112ddd3a12fd7b588a1bf56a4f39ce0b94559
Victor chikezie <vchikezie50@gmail.com>
2023-07-26 00:54:51 +0100

    new

--- screens/About.js
+++ screens/About.js
@@ -3,11 +3,10 @@ import { useState,useCallback,useEffect } from "react";
 import { SafeArea } from "../components/SafeArea";
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
-import {Pacifico_400Regular} from '@expo-google-fonts/pacifico';
+import { Pacifico_400Regular } from "@expo-google-fonts/pacifico";
 import { Theme } from "../utils/Theme";
 
-
-export function About ({navigation}) {
+export function About () {
     const [appIsReady, setAppIsReady] = useState(false);
 
     useEffect(() => {
@@ -25,10 +24,10 @@ export function About ({navigation}) {
           setAppIsReady(true);
         }
       }
-
+  
       prepare();
     }, []);
-
+  
     const onLayoutRootView = useCallback(async () => {
       if (appIsReady) {
         // This tells the splash screen to hide immediately! If we call this after
@@ -39,91 +38,79 @@ export function About ({navigation}) {
         await SplashScreen.hideAsync();
       }
     }, [appIsReady]);
-
+  
     if (!appIsReady) {
       return null;
     }
     return (
         <SafeArea>
-            <View style={styles.color}>
-            <View style={styles.com}>
-                <Text style={styles.title}>Charity App</Text>
-                <Text style={styles.course}>Donate to a worthy course</Text>
-                <View style={styles.border}>
-                    <Text style={styles.text}>
-                      The Charity App
-                      Foundation's mission-unchanged 
-                      since 1913. is to promote the wellbeing 
-                      of humanity throughout the world.
-                      Today the Foundation 
-                      advances new frontiers of 
-                      science, data, policy, and 
-                      innovation to solve global 
-                      challenges related to health, food, power, and 
-                      economic mobility
+            <View style={style.color}>
+            <View style={style.com}>
+                <Text style={style.title}>Charity App</Text>
+                <Text style={style.course}>Donate to a worthy course</Text>
+                <View style={style.border}>
+                    <Text style={style.text}>
+                    The Charity App
+                    Foundation's mission-unchanged 
+                    since 1913. is to promote the wellbeing 
+                    of humanity throughout the world.
+                    Today the Foundation 
+                    advances new frontiers of 
+                    science, data, policy, and 
+                    innovation to solve global 
+                    challenges related to health, food, power, and 
+                    economic mobility
                     </Text>
-                    
                 </View>
-                <TouchableOpacity onPress={() => navigation.navigate('Donate')}>
-                  <View style={styles.donationBox}>
-                    <Text style={styles.text}>
-                      Make A Donation
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-            
+                <View style={style.button}>
+                <Button
+                    title="Make a Donation"
+                    color='white'
+                    onPress={() => Alert.alert('Simple Button pressed')}
+                /></View>
             </View>
-           
             </View>
         </SafeArea>
-  );
-
+    );
+   
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
     color:{
         flex:1,
         //backgroundColor:(Theme.colors.purple100)
     },
-
     com:{
         flexDirection:'column',
         justifyContent:'center',
         alignItems:'center',
         marginTop:50
     },
-
     title:{
         fontSize:35,
         fontFamily:'Pacifico_400Regular'
-    },
-    
-    course:{
+    },course:{
         marginTop:15,
         fontSize:20
     },
-
     border:{
         borderWidth:0.2,
         borderRadius:8,
         marginTop:10,
-        backgroundColor:Theme.colors.gray400,
+        backgroundColor:'blue',
         padding:30,
         margin:50
     },
-
     text:{
         fontSize:20,
         color:'white'
     },
-
-    donationBox:{
-      backgroundColor:Theme.colors.purple900,
-      paddingHorizontal:15,
-      paddingVertical:15,
-      borderRadius:5,
-      marginBottom:3,
-      marginTop:15,
-      justifyContent:'center'
+    button:{
+        borderWidth:1,
+        borderRadius:5,
+        padding:7,
+        width:270,
+        backgroundColor:'blue'
     }
+    
 })
--- screens/Donate.js
+++ screens/Donate.js
@@ -1,25 +1,22 @@
-import { useContext, } from "react";
-import { AppContext } from "../settings/globalVariables";
+import { useContext } from "react";
+import {AppContext} from "../settings/globalVariables";
 import { StyleSheet } from "react-native";
 import { SafeArea } from "../components/SafeArea";
-import { Theme } from "../utils/Theme";
-
-
+import {Theme} from "../utils/Theme"
 
 export function Donate () {
-    const {uid} = useContext(AppContext);
-    console.log(uid);
+    const { uid,setFirstName } = useContext(AppContext);
+
     return (
         <SafeArea>
-
+          
         </SafeArea>
     )
 }
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
     title:{
         color:Theme.colors.brown300,
         fontSize:Theme.sizes[4]
     }
 })
-
